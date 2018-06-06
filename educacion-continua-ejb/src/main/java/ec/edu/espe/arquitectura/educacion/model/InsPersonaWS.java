4f714a64a32446d38edee43c9408dceac253471b
jose cajape <jscajape@espe.edu.ec>
2018-06-05 19:42:23 -0500

    insPersonaWS

---
+++ educacion-continua-ejb/src/main/java/ec/edu/espe/arquitectura/educacion/model/InsPersonaWS.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.arquitectura.educacion.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author js_cm
+ */
+public class InsPersonaWS {
+    
+    private String codigo;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String direccion;
+
+    private String telefono;
+
+    private Date fecNac;
+
+    private String tipo;
+
+    private String genero;
+    
+    private String correoElectronico;
+    
+    private String estadoUsuario;
+
+    public InsPersonaWS() {
+    }
+    
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Date getFecNac() {
+        return fecNac;
+    }
+
+    public void setFecNac(Date fecNac) {
+        this.fecNac = fecNac;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getEstadoUsuario() {
+        return estadoUsuario;
+    }
+
+    public void setEstadoUsuario(String estadoUsuario) {
+        this.estadoUsuario = estadoUsuario;
+    }
+
+    
+    
+}
